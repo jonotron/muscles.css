d607150af8ee52803cf9e569adfb2c924f23bf74
Jonathan Bowers <jonotron@gmail.com>
2015-11-24 09:41:00 -0800

    separate styles into logical modules

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 
 gulp.task('css', function() {
-  return gulp.src('muscles/**/*.css')
-    .pipe(postcss([ require('postcss-for') ]))
+  return gulp.src('muscles/muscles.css')
+    .pipe(postcss([ require('postcss-import'), require('postcss-for') ]))
     .pipe(gulp.dest('dist/'));
 });
 
