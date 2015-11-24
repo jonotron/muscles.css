e3f300aaa03e47665da53a7293df66edeeb0c4e3
Jonathan Bowers <jonotron@gmail.com>
2015-11-23 19:21:29 -0800

    remove sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,4 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 
-gulp.task('default', ['sass']);
+gulp.task('default');
 
-gulp.task('sass', function() {
-  return gulp.src('sass/**/*.scss')
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    .pipe(gulp.dest('dist'));
-});
