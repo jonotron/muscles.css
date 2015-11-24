b4278dbf25322047931baa53b03c3db5e6d957fc
Jonathan Bowers <jonotron@gmail.com>
2015-11-23 19:38:08 -0800

    run muscles through postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 var gulp = require('gulp');
+var postcss = require('gulp-postcss');
 
-gulp.task('default');
+gulp.task('css', function() {
+  return gulp.src('muscles/**/*.css')
+    .pipe(postcss([ require('postcss-for') ]))
+    .pipe(gulp.dest('dist/'));
+});
 
