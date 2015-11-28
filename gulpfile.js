9c0659682cce1676638d5dc184563165018bf0a3
Jonathan Bowers <jonotron@gmail.com>
2015-11-28 11:19:06 -0800

    use postcss-simple-vars in processors

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,13 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 
 gulp.task('css', function() {
+  var processors = [
+    require('postcss-import'),
+    require('postcss-for'),
+    require('postcss-simple-vars') // must come after postcss-for
+  ];
   return gulp.src('muscles/muscles.css')
-    .pipe(postcss([ require('postcss-import'), require('postcss-for') ]))
+    .pipe(postcss(processors))
     .pipe(gulp.dest('dist/'));
 });
 
