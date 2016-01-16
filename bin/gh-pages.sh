964f765e22c31eea86fc922b922bbd3a06c0df2d
Jonathan Bowers <jonotron@gmail.com>
2016-01-15 16:20:27 -0800

    simple gh-pages script to automatically push gh-pages

---
+++ bin/gh-pages.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+remote="git@github.com/jonotron/muscles.css.git"
+
+git checkout gh-pages
+git pull $remote gh-pages
+git merge master
+npm run mdcss
+git add .
+git commit -m "deploy from $WERCKER_STARTED_BY"
+result="$(git push $remote gh-pages:gh-pages)"
+
+if [[ $? -ne 0]]
+then
+  warning "$result"
+  fail "failed pushing to github pages"
+else
+  success "pushed to github pages"
+fi
