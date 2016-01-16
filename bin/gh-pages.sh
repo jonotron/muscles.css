c4812fd218ceed8b2941644d7133f1a9e1285feb
Jonathan Bowers <jonotron@gmail.com>
2016-01-15 16:24:36 -0800

    fix deploy issues

--- bin/gh-pages.sh
+++ bin/gh-pages.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-remote="git@github.com/jonotron/muscles.css.git"
+remote="git@github.com:/jonotron/muscles.css.git"
 
 git checkout gh-pages
-git pull $remote gh-pages
 git merge master
 npm run mdcss
 git add .
 git commit -m "deploy from $WERCKER_STARTED_BY"
-result="$(git push $remote gh-pages:gh-pages)"
+result="$(git push -f $remote gh-pages:gh-pages)"
 
 if [[ $? -ne 0]]
 then
