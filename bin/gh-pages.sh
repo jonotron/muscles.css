16b632999d518382fe1fe0e2653315e56a388be8
Jonathan Bowers <jonotron@gmail.com>
2016-04-19 16:47:03 -0700

    remove the checks at end of gh-pages deploy

--- bin/gh-pages.sh
+++ bin/gh-pages.sh
@@ -12,13 +12,4 @@ git config user.email "jonotron@gmail.com"
 git config user.name "Semaphore CI Bot"
 git add .
 git commit -m "deploy new gh-pages"
-result="$(git push -f git@github.com:/jonotron/muscles.css.git master:gh-pages)"
-
-if [[ $? -ne 0 ]]
-then
-  echo "$result"
-  exit 1 
-else
-  echo "pushed to github pages"
-fi
-
+git push -f git@github.com:/jonotron/muscles.css.git master:gh-pages
